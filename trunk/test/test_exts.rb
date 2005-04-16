5b213dfe0dc48998476b599b55c27bfc08bde653
dm1 <dm1@1ccd8071-c7f4-0310-8b09-a7971b518007>
2005-04-16 19:35:17 +0000

    add String#strip_q tests

--- trunk/test/test_exts.rb
+++ trunk/test/test_exts.rb
@@ -17,3 +17,22 @@ class StringInterpolateTest < Test::Unit::TestCase
     assert_equal 'the horse is blue', string.interpolate(binding)
   end
 end
+
+class StringStrip_qTest < Test::Unit::TestCase
+
+  def test_strip_q
+    assert_equal 'blah bla bla  ', '   "blah bla bla  " '.strip_q
+  end
+  
+  def test_strip_q!
+    s = '   "blah bla bla  " '
+    s.strip_q!
+    assert_equal 'blah bla bla  ', s
+  end
+
+# TODO: make this work (even if it is not needed in the code for now)
+  def _test_strip_q_incomplete
+    assert_equal 'blah bla bla', '   "blah bla bla '.strip_q
+  end
+
+end
